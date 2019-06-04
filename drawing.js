e58651fd724466cc29bf34f6e47fd2cfdae3c6f8
twohiccups <koshkinmark@gmail.com>
2019-06-04 04:44:32 -0400

    update

---
+++ drawing.js
@@ -0,0 +1,124 @@
+var ns = 'http://www.w3.org/2000/svg';
+var canvas = document.getElementById('triangulation');
+var button = document.getElementById('button');
+var labels = [];
+var pt = canvas.createSVGPoint();
+var boundingBox = canvas.getBoundingClientRect();
+
+
+function resizing() {
+    pt = canvas.createSVGPoint();
+    boundingBox = canvas.getBoundingClientRect();
+}
+
+window.onresize = resizing;
+
+
+
+// Get point in global SVG space
+function cursorPoint(evt) {
+  pt.x = evt.clientX - boundingBox.left;
+  pt.y = evt.clientY - boundingBox.top;
+  return pt.matrixTransform(canvas.getScreenCTM().inverse());
+}
+
+canvas.addEventListener('click',function(evt){
+  var loc = cursorPoint(evt);
+  // Use loc.x and loc.y here
+},false);
+
+canvas.addEventListener('click', function(e) {
+    points.push(createPoint(pt.x, pt.y));
+    if (points.length > 1) {
+        connectPoints(points[points.length-1], points[points.length-2]);
+    }
+});
+
+button.addEventListener('click', function() {
+    console.log(points);
+    triangulate(points);
+
+    if (points.length > 2) {
+        connectPoints(points[points.length-1], points[0]);
+    }
+    else {
+        alert('Needs at least three points');
+    }
+});
+
+
+function labelPoints(points) {
+        for(var i = 0; i < points.length; i++) {
+            var textNode = document.createElementNS(ns, 'text');
+            textNode.setAttributeNS(null, 'x', points[i].x + 10);
+            textNode.setAttributeNS(null, 'y', points[i].y + 10);
+            textNode.setAttributeNS(null, 'fill', 'red');
+            textNode.setAttributeNS(null, 'family', 'sans-serif');
+            textNode.setAttributeNS(null, 'font-size', '20px');
+            var text = document.createTextNode(i);
+            textNode.appendChild(text);
+            canvas.appendChild(textNode);
+            labels.push(textNode);
+            points[i].id = i;
+        }
+}
+
+function toggleLables() {
+    if (labels == []) return;
+    var fill;
+    if (labels[0].getAttribute('fill') == 'red') {
+        fill = 'transparent';
+    }
+    else {
+        fill = 'red';
+    }
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].setAttributeNS(null, 'fill', fill); 
+    }
+}
+
+function connectPoints(a, b) {
+    var line = document.createElementNS(ns, 'line');
+    line.setAttributeNS(null, 'x1', a.x);
+    line.setAttributeNS(null, 'y1', a.y);
+    line.setAttributeNS(null, 'x2', b.x);
+    line.setAttributeNS(null, 'y2', b.y);
+    line.setAttributeNS(null, 'stroke', 'black'); 
+    canvas.appendChild(line);
+    return line;
+}
+
+function drawTriangle(triangle) {
+    var polygon = document.createElementNS(ns, 'polygon');
+    polygon.setAttributeNS(null, 'points', triangle.a.x + ','+ triangle.a.y + ' ' +
+                                           triangle.b.x +',' + triangle.b.y + ' ' +
+                                           triangle.c.x +',' + triangle.c.y);
+    polygon.setAttributeNS(null, 'fill', getRandomColor()); 
+    polygon.setAttributeNS(null, 'stroke', 'black'); 
+    canvas.insertBefore(polygon, canvas.childNodes[0]);
+    return polygon;
+}
+
+function getRandomColor() {
+  var letters = 'BCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 5)];
+  }
+  return color;
+}
+
+function drawTriangles(triangles) {
+    function setDelay(i) {
+        setTimeout(function(){
+            drawTriangle(triangles[i]);
+        }, i * 1000);
+    }   
+    for (var i = 0; i < triangles.length; i++) {
+        setDelay(i); 
+    } 
+}
+
+function clearCanvas() {
+     canvas.innerHTML = '';
+}
---
+++ triangulation.js
@@ -0,0 +1,114 @@
+
+var points = [];
+const interval = 750;
+
+
+// point with lowest y coordinate
+// given collision, pick the one with smallest x
+// in svg, coordinates are reversed
+
+function Point(x, y, c) {
+    this.x = x;
+    this.y = y;
+    this.c = c;
+}
+
+function createPoint(x, y) {
+    var cir = document.createElementNS(ns, 'circle');
+    cir.setAttributeNS(null, 'cx', x);
+    cir.setAttributeNS(null, 'cy', y);
+    cir.setAttributeNS(null, 'r', 4);
+    cir.setAttributeNS(null, 'fill', 'black');
+    canvas.appendChild(cir);
+    var point = new Point(x,y,cir);
+    return point;
+}
+
+function distanceSq(a, b) {
+    return (a.x-b.x) * (a.x-b.x) + (a.y-b.y) * (a.y-b.y); 
+}
+
+function angle(a, b, c) {
+    var temp =  Math.atan2((c.y-b.y), (c.x-b.x)) - Math.atan2((a.y-b.y), (a.x-b.x));
+    return temp < 0 ? Math.PI*2 + temp: temp;
+}
+
+function Triangle(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.area = function() {
+        return (a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y))/2;
+    }
+}
+
+
+
+
+function measureAngles(points) {
+    for (var i = 0; i < points.length+1; i++) {
+        points[(i+1) % points.length].angle = angle(points[i% points.length],
+                                  points[(i+1) % points.length],
+                                  points[(i+2) % points.length]) * 180 / Math.PI;
+    }
+    
+}
+
+function isInsideTriangle(triangle, point) {
+    var areaA, areaB, areaC;
+    areaA = (new Triangle(triangle.a, triangle.b, point)).area();
+    areaB = (new Triangle(triangle.b, triangle.c, point)).area();
+    areaC = (new Triangle(triangle.c, triangle.a, point)).area();
+    return  (Math.sign(areaA) == Math.sign(areaB) &&
+             Math.sign(areaA) == Math.sign(areaC));
+}
+
+
+function triangulate(points) {
+
+    if (points < 3) return;
+    
+     if (angle(points[0], points[1], points[2]) > Math.PI) {
+          points.reverse();
+      }
+    
+    var i = 1;
+    var a,b,c;
+    var triangles = [];
+    
+    labelPoints(points);
+    measureAngles(points);
+    while(points.length > 3) {
+        a = points[i % points.length];
+        b = points[(i+1) % points.length];
+        c = points[(i+2) % points.length];
+        
+        if (angle(a,b,c) < Math.PI) {
+            var tempTriangle = new Triangle(a, b, c);
+            var isEar = true;
+            for(var j = i+3; j < points.length + i + 3; j++) {
+                if (isInsideTriangle(tempTriangle,points[j % points.length]))
+                    isEar = false;
+            }
+            if (isEar) {
+                points.splice((i+1) % points.length,1); // erase middle point
+                triangles.push(tempTriangle);
+            }
+        }
+        i++; 
+        if (i > points.length * points.length * points.length) break;
+    } 
+    triangles.push(new Triangle(points[0], points[1], points[2]));
+    setTimeout(function () {drawTriangles(triangles);}, interval);
+
+}
+
+function reset() {
+    points = [];
+    clearCanvas();
+}
+
+
+
+
+
